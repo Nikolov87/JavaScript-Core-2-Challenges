21f71914bd2806d8f81819127c13363b871c9a6f
Nikolov87 <100172752+Nikolov87@users.noreply.github.com>
2022-10-22 09:42:45 +0100

    WM4-core2-challenges-Nikolov87

--- challenge-count-words/script.js
+++ challenge-count-words/script.js
@@ -2,7 +2,17 @@ function calculateWords(chapterOfABook) {
   const wordCount = {};
 
   // Write your code in here
+  let split = chapterOfABook.split(" ");
 
+  split.forEach((word) => {
+    if (word !== "") {
+      if (wordCount[word] === undefined) {
+        wordCount[word] = 1;
+      } else {
+        wordCount[word] += 1;
+      }
+    }
+  });
   return wordCount;
 }
 
--- challenge-fastest-presser/script.js
+++ challenge-fastest-presser/script.js
@@ -1,11 +1,59 @@
-function startGame() {}
+let playerOneScore = 0;
+let playerTwoScore = 0;
 
-function keyBoardEvents(e) {
-  if (e.keyCode === 83) {
-    // On 'S' Pressed
-  } else if (e.keyCode === 76) {
-    // On 'L' Pressed
+let gameRunning = false;
+
+const playerOneScoreText = document.getElementById("playerOneScore");
+const playerTwoScoreText = document.getElementById("playerTwoScore");
+const wonText = document.getElementById("wonText");
+const input = document.getElementById("timerInput");
+let startButton = document.getElementById("startButton");
+startButton.addEventListener("click", startGame);
+
+document.addEventListener("keypress", keyBoardEvents);
+
+function startGame() {
+  restartGame();
+
+  let time = input.value * 1000;
+  setTimer(time);
+  gameRunning = true;
+}
+
+function restartGame() {
+  wonText.innerText = "";
+  playerOneScore = 0;
+  playerTwoScore = 0;
+  playerOneScoreText.innerText = "Score: " + playerOneScore;
+  playerTwoScoreText.innerText = "Score: " + playerTwoScore;
+}
+
+function setTimer(time) {
+  setTimeout(function () {
+    gameRunning = false;
+    setWinner();
+  }, time);
+}
+
+function setWinner() {
+  if (playerOneScore > playerTwoScore) {
+    wonText.innerText = "Player One Won!!";
+  } else if (playerOneScore < playerTwoScore) {
+    wonText.innerText = "Player Two Won!!";
+  } else if (playerOneScore === playerTwoScore) {
+    wonText.innerText = "Players Tied!";
   }
 }
 
-document.addEventListener("keypress", keyBoardEvents);
+function keyBoardEvents(e) {
+  if (gameRunning) {
+    if (e.keyCode === 115) {
+      playerOneScore++;
+    } else if (e.keyCode === 108) {
+      playerTwoScore++;
+    }
+
+    playerOneScoreText.innerText = "Score: " + playerOneScore;
+    playerTwoScoreText.innerText = "Score: " + playerTwoScore;
+  }
+}
